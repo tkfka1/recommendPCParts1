3cae9149c50f65dc957cc4f19b92c85279bf5896
tkfka1 <36651040+tkfka1@users.noreply.github.com>
2022-10-27 08:03:24 +0900

    기본 페이지 및 ajax 데이터받기

--- app.py
+++ app.py
@@ -1,13 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify,request
+
 app = Flask(__name__)
 
+
+# 기본 index
 @app.route('/')
 def home():
-   return 'This is Home!'
+   return render_template('index.html')
 
-@app.route('/mypage')
-def mypage():
-   return 'This is My Page!'
+# API
+@app.route('/api/list', methods=['POST'])
+def show_recommend():
+   # ajax 데이터 받아오기
+   data_dic = {
+      'cpu': request.form['Cpu'],
+      'gpu': request.form['Gpu'],
+      'purpose': request.form['Purpose'],
+      'ssd': request.form['Ssd'],
+      'Hdd': request.form['Hdd'],
+      'price': request.form['Price'],
+               }
+   print(data_dic)
+   return jsonify({'result': 'success', 'msg': 'list 연결되었습니다!'})
 
 if __name__ == '__main__':
-   app.run('0.0.0.0',port=5000,debug=True)
+   app.run('0.0.0.0', port=5000, debug=True)
